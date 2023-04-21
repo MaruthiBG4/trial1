bbb95fc486c674b80296757aa0fe3203d3ba8a9b
MaruthiBG <bgmaruthi2004@gmail.com>
2023-04-21 12:55:51 +0530

    first commit

---
+++ wrapper/wrapperExample/src/wrapperExamplepack/Wrapper.java
@@ -0,0 +1,33 @@
+package wrapperExamplepack;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		/*
+		 * int a= 20; Integer i = Integer.valueOf(a); //converting int to integer//
+		 * Integer j = a; System.out.println(a+ " " +i+" " +j);
+		 */
+		
+		
+		String str = "12";
+		int num3 = Integer.parseInt(str);
+		System.out.println(num3*2);
+		/*
+		 * public static toInt(String s) { return Integer.parseInt(s); //to get the
+		 * corresponding primitive int value }
+		 */
+		
+		
+		
+		/*
+		 * public static void main(String args[]) { int x = 5; float y = 3.14f; long
+		 * z=6000;
+		 * 
+		 * // autoboxing Integer intobj = x; Float floatobj=y; Long longobj=z; // print
+		 * System.out.println(intobj); System.out.println(floatobj);
+		 * System.out.println(longobj); }
+		 */
+
+	}
+
+}
